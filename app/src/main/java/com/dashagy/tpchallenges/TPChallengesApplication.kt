310ccd3f1df07ff47a40ad08be55a45695c90c92
Juan Jose Abuin <juanj.abuin@gmail.com>
2023-03-31 18:29:41 -0300

    Testing fusedLocationProvider

--- app/src/main/java/com/dashagy/tpchallenges/TPChallengesApplication.kt
+++ app/src/main/java/com/dashagy/tpchallenges/TPChallengesApplication.kt
@@ -1,6 +1,10 @@
 package com.dashagy.tpchallenges
 
 import android.app.Application
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.content.Context
+import android.os.Build
 import com.google.firebase.FirebaseApp
 
 import dagger.hilt.android.HiltAndroidApp
@@ -9,6 +13,17 @@ import dagger.hilt.android.HiltAndroidApp
 class TPChallengesApplication: Application() {
     override fun onCreate() {
         super.onCreate()
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val channel = NotificationChannel(
+                "channel_id",
+                "channel_name",
+                NotificationManager.IMPORTANCE_DEFAULT
+            )
+            val notificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+            notificationManager.createNotificationChannel(channel)
+        }
+
         FirebaseApp.initializeApp(this)
     }
 }
--- app/src/main/java/com/dashagy/tpchallenges/presentation/fragments/MapFragment.kt
+++ app/src/main/java/com/dashagy/tpchallenges/presentation/fragments/MapFragment.kt
@@ -1,11 +1,18 @@
 package com.dashagy.tpchallenges.presentation.fragments
 
+import android.Manifest
+import android.annotation.SuppressLint
 import android.os.Bundle
+import android.os.Looper
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
 import com.dashagy.tpchallenges.databinding.FragmentMapBinding
+import com.google.android.gms.location.*
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -14,12 +21,91 @@ class MapFragment : Fragment() {
     private var _binding: FragmentMapBinding? = null
     private val binding get() = _binding!!
 
+    private lateinit var locationPermissionLauncher: ActivityResultLauncher<Array<String>>
+
+    private lateinit var fusedLocationProviderClient: FusedLocationProviderClient
+
+    private lateinit var locationRequest: LocationRequest
+    private lateinit var locationCallback: LocationCallback
+    private var lastLocation: LocationResult? = null
+
+    private var isServiceStarted: Boolean = false
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        locationPermissionLauncher = registerLocationPermission()
+        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(requireContext())
+    }
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         _binding = FragmentMapBinding.inflate(inflater, container, false)
+
+        binding.btnStartLocationService.setOnClickListener { launchLocationPermission() }
+        binding.btnStopLocationService.setOnClickListener { stopLocationUpdates() }
+
         return binding.root
+    }
+
+    private fun registerLocationPermission() =
+        registerForActivityResult(ActivityResultContracts.RequestMultiplePermissions()){
+            val areAllPermissionsGranted = it.any { (_, value) -> value }
+            if (areAllPermissionsGranted) {
+                startLocationUpdates()
+            } else {
+                Toast.makeText(requireActivity(), "Permissions rejected", Toast.LENGTH_SHORT).show()
+            }
+    }
+
+    private fun launchLocationPermission() {
+        locationPermissionLauncher.launch(
+            arrayOf(
+                Manifest.permission.ACCESS_COARSE_LOCATION,
+                Manifest.permission.ACCESS_FINE_LOCATION
+            )
+        )
+    }
+
+    @SuppressLint("MissingPermission", "VisibleForTests")
+    private fun startLocationUpdates() {
+        locationCallback = object : LocationCallback() {
+            override fun onLocationResult(result: LocationResult?) {
+                updateLastLocation(result)
+            }
+        }
+
+        locationRequest = LocationRequest.create().apply {
+            priority = LocationRequest.PRIORITY_HIGH_ACCURACY
+            interval = 10_000
+            fastestInterval = 5_000
+        }
+
+        fusedLocationProviderClient.requestLocationUpdates(
+            locationRequest,
+            locationCallback,
+            Looper.getMainLooper()
+        )
+
+        updateIsServiceStarted(true)
+    }
+
+    private fun stopLocationUpdates() {
+        fusedLocationProviderClient.removeLocationUpdates(locationCallback)
+        updateIsServiceStarted(false)
+        updateLastLocation(null)
+    }
+
+    private fun updateIsServiceStarted(value: Boolean){
+        isServiceStarted = value
+        binding.btnStopLocationService.isEnabled = value
+    }
+
+    private fun updateLastLocation(location: LocationResult?){
+        lastLocation = location
+        binding.tvLocation.text = location?.let { "Lat: ${it.lastLocation.latitude}, Lon: ${it.lastLocation.longitude}" } ?: "Lat: , Lon:"
     }
 
     companion object {
